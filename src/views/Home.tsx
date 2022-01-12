bf74179af40ab1db18a4d661830645b061e06bfa
Haddee Ann <haddeeann@gmail.com>
2022-01-11 19:54:36 -0500

    just tooling around

---
+++ src/views/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from '@mui/material'
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <Button variant='contained'>Press the goobery bitch button</Button>
+                and this is a story all about how
+                <span>my life got flipped and turned upside down.</span>
+            </div>
+            <div>
+                goobery bitches
+            </div>
+        </div>
+    );
+}
+
+export default Home;
