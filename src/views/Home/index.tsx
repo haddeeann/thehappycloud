2ea9b4c5317ce73867dffdaece20d53e30096062
Haddee Ann <haddeeann@gmail.com>
2022-01-12 12:16:56 -0500

    woot, scrolling looking pretty

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -8,22 +8,63 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
-    const el = useRef();
+    const aRef = useRef();
+    const bRef = useRef();
+    const cRef = useRef();
+    const dRef = useRef();
+    const eRef = useRef();
+    const fRef = useRef();
+    const gRef = useRef();
+    const mainRef = useRef();
+    // const q = gsap.utils.selector(mainRef);
     useEffect(() => {
-        gsap.to(el.current, {
-            scrollTrigger: {
-                trigger: el.current,
-                start: 'top top',
-                markers: true,
-                pinSpacing: false,
-                pin: true,
-            }, 
+        let arrRef = [aRef, bRef, cRef, dRef, eRef, fRef, gRef];
+        for(let el of arrRef) {
+            gsap.to(el.current, {
+                scrollTrigger: {
+                    trigger: el.current,
+                    start: 'top top',
+                    markers: true,
+                    pinSpacing: false,
+                    pin: true,
+                }, 
+            });
+        }
+        //  const sOne = gsap.to(el.current, {
+        //     scrollTrigger: {
+        //         trigger: el.current,
+        //         start: 'top top',
+        //         markers: true,
+        //         pinSpacing: false,
+        //         pin: true,
+        //     }, 
 
-        })
-    });
+        // });
+
+        // const sTwo = gsap.to(em.current, {
+        //     scrollTrigger: {
+        //         trigger: em.current,
+        //         start: 'top top',
+        //         markers: true,
+        //         pinSpacing: false,
+        //         pin: true,
+        //     }, 
+
+        // });
+
+        // const panel = gsap.to(q('.panel'), {
+        //     opacity: '0.35'
+        // });
+
+        return () => {
+            // sOne.kill();
+            // sTwo.kill();
+            // panel.kill();
+        }
+    }, []);
     return (
-        <main>
-            <section className={`${styles.section} ${styles.grayOne} panel`} ref={el}>
+        <main ref={mainRef}>
+            <section className={`${styles.section} ${styles.grayOne} panel`} ref={aRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     A is for Apple
                 </Typography>
@@ -37,7 +78,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section className={`${styles.section} ${styles.grayTwo} panel`}>
+            <section className={`${styles.section} ${styles.grayTwo} panel`} ref={bRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     B is for Button
                 </Typography>
@@ -51,7 +92,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section className={`${styles.section} ${styles.grayThree} panel`}>
+            <section className={`${styles.section} ${styles.grayThree} panel`} ref={cRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     C is for Crayons
                 </Typography>
@@ -65,7 +106,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section  className={`${styles.section} ${styles.grayFour} panel`}>
+            <section  className={`${styles.section} ${styles.grayFour} panel`} ref={dRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     D is for Doggos
                 </Typography>
@@ -79,7 +120,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section className={`${styles.section} ${styles.grayFive} panel`}>
+            <section className={`${styles.section} ${styles.grayFive} panel`} ref={eRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     E is for Elephants
                 </Typography>
@@ -93,7 +134,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section className={`${styles.section} ${styles.graySix} panel`}>
+            <section className={`${styles.section} ${styles.graySix} panel`} ref={fRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     F is for Fungi
                 </Typography>
@@ -107,7 +148,7 @@ const Home = () => {
                     and this is a story all about how <span> my life got flipped and turned upside down.</span>
                 </Typography>
             </section>
-            <section className={`${styles.section} ${styles.graySeven} panel`}>
+            <section className={`${styles.section} ${styles.graySeven} panel`} ref={gRef}>
                 <Typography variant='h2' component='div' gutterBottom>
                     G is for Ghosts and Ghouls
                 </Typography>
